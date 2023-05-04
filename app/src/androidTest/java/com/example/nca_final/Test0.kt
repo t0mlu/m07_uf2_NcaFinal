a265bce190818ccca9c5ab8c7e6f0f6203b3b24e
Tomàs Lucas <tomaslucasmendez@gmail.com>
2023-05-04 19:47:29 +0200

    Test 0 working

---
+++ app/src/androidTest/java/com/example/nca_final/Test0.kt
@@ -0,0 +1,14 @@
+package com.example.nca_final
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+@RunWith(Suite::class)
+@SuiteClasses(
+    Test1::class,
+    Test2::class,
+    Test3::class,
+    Test4::class
+)
+class Test0
